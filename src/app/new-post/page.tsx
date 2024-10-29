8858a201b6ad36747dead8424e54ea2e93eeb772
tomas <tomas@kislan.sk>
2024-10-29 09:17:28 +0000

    Add react hook form, add create post page, add create post server action

---
+++ src/app/new-post/page.tsx
@@ -0,0 +1,14 @@
+import { NewPostForm } from "./NewPostForm";
+
+export default async function NewPostPage() {
+  console.log("NewPostPage");
+
+  return (
+    <div className="card bg-base-100 w-96 drop-shadow-md">
+      <div className="card-body">
+        <p>New Post</p>
+        <NewPostForm />
+      </div>
+    </div>
+  );
+}
